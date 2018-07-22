1f859044c4115531faa0ec20f2f60a33e7110333
Skip Sopscak <dsopscak@gmail.com>
2018-07-22 13:36:02 -0500

    Add utility to sync local stuff with a (possibly shared) backup.

---
+++ bin/rsync_with_cloud.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#
+# Supports an encrypted tarball representing a (possibly shared)
+# backup of some set of files and directories. Updates local "source"
+# first with anything newer in the backup. Then updates the backup
+# with anyting newer from the source. Will possibly lead to amazement
+# if the same file is updated in different sources before either does
+# an update.
+
+set -e
+set -x
+
+if [[ -z "$1" || -z "$2" || -z "$3" ]]; then
+    echo "usage: rsync_with_cloud.sh <key> <target> <file|dir ...>"
+    exit 1
+fi
+
+MMI_RSYNC_KEY=$1
+MMI_RSYNC_TGT=$2
+
+temp_work="/tmp/rsync_with_cloud.$$"
+mkdir $temp_work
+
+if [[ -e $MMI_RSYNC_TGT ]]; then
+    # decrypt into workarea and update source
+    gpg --passphrase $MMI_RSYNC_KEY -d -o- $MMI_RSYNC_TGT |\
+        tar -xzv -C $temp_work
+    for f in ${@:3}; do
+        rsync -avuP "$temp_work/$f/" "$f"
+    done
+fi
+
+for f in ${@:3}; do
+    # rsync into workarea
+    rsync -avuP "$f" "$temp_work"
+done
+
+# encrypt workarea into target
+cd $temp_work
+tar czf - * | gpg --yes --passphrase $MMI_RSYNC_KEY -c -o "$MMI_RSYNC_TGT"    
