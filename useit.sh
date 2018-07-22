74e0ca428eb82689adc895ecfdad97e00cc5bb3b
Skip Sopscak <dsopscak@gmail.com>
2018-07-22 16:49:36 -0500

    Add bin to list of installables

--- useit.sh
+++ useit.sh
@@ -4,7 +4,7 @@ if [[ $1 == "-f" ]]; then FORCE=true; fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-for f in .vim .bashrc .bash_profile .vimrc; do
+for f in bin .vim .bashrc .bash_profile .vimrc; do
     if [[ -z "$FORCE" &&  -e $HOME/$f ]]; then
         echo "$HOME/$f exists, run with -f to force overwrite"
         exit 1
