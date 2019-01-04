e54e15881cf766f9631c06163c5e127e2d7e31f3
Skip Sopscak <dsopscak@gmail.com>
2019-01-03 21:53:45 -0600

    make cryto key an env var

--- bin/setup1.sh
+++ bin/setup1.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
-rsync_with_cloud.sh foobar fedora_vm.rwc .ssh .vim .vimrc .Xdefaults .Xresources .bash_profile .bashrc
+rsync_with_cloud.sh $crypto_key fedora_vm.rwc .ssh .vim .vimrc .Xdefaults .Xresources .bash_profile .bashrc
 git config --global --replace-all user.email dsopscak@gmail.com 
 git config --global --replace-all  user.name "Skip Sopscak"
